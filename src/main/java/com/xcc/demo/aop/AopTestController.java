57ffadd84eb988a6309be54112a9276cfcc1723e
xiechaochao <xiechaochao@shein.com>
2019-06-12 18:53:54 +0800

    修改提交

--- src/main/java/com/xcc/demo/aop/AopTestController.java
+++ src/main/java/com/xcc/demo/aop/AopTestController.java
@@ -1,10 +1,15 @@
 package com.xcc.demo.aop;
 
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.*;
 import com.xcc.comm.annotation.Sign;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.xcc.demo.test.dao.PdfDto;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import com.itextpdf.text.Document;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,4 +39,46 @@ public class AopTestController {
         }
         return resultMap;
     }
+
+    @PostMapping(value = "/mark")
+    @ResponseBody
+    public String  selectSiteEntity(@Validated PdfDto dto) {
+
+        // 模板路径
+        String templatePath = "D:/a.pdf";
+        // 生成的新文件路径
+        String newPDFPath = "D:/f.pdf";
+        PdfReader reader;
+        FileOutputStream out;
+        ByteArrayOutputStream bos;
+        PdfStamper stamper;
+        try {
+            out = new FileOutputStream(newPDFPath);// 输出流
+            reader = new PdfReader(templatePath);// 读取pdf模板
+            bos = new ByteArrayOutputStream();
+            stamper = new PdfStamper(reader, bos);
+            AcroFields form = stamper.getAcroFields();
+
+            form.setField("debentures",dto.getTitle());
+            form.setField("debenturesLegalRepresentative",dto.getAge());
+            form.setField("debenturesAddress",dto.getName());
+
+            stamper.setFormFlattening(true);// 如果为false那么生成的PDF文件还能编辑，一定要设为true
+            stamper.close();
+
+            Document doc = new Document();
+            PdfCopy copy = new PdfCopy(doc, out);
+            doc.open();
+            PdfImportedPage importPage = copy.getImportedPage(new PdfReader(bos.toByteArray()), 1);
+            copy.addPage(importPage);
+            doc.close();
+
+        } catch (IOException e) {
+            System.out.println(1);
+        } catch (DocumentException e) {
+            System.out.println(2);
+        }
+        return "";
+    }
+
 }
---
+++ src/main/java/com/xcc/demo/test/dao/PdfDto.java
@@ -0,0 +1,10 @@
+package com.xcc.demo.test.dao;
+
+import lombok.Data;
+
+@Data
+public class PdfDto {
+    private String title;
+    private String name;
+    private String age;
+}
